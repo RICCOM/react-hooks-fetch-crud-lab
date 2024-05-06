006827a90cbe0edc48ab3ec0fa308486e10c0521
RICCOM <ericomun@gmail.comgit>
2024-05-06 20:31:31 +0300

    useState & useEffect

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({ questions }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(question => (
+          <li key={question.id}>
+            <h2>{question.prompt}</h2>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
